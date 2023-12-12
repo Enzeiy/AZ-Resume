05fda1029726a444dabc6f9b74413aa881a88437
Enzeiy <134259290+Enzeiy@users.noreply.github.com>
2023-12-13 01:57:36 +0800

    update main.js and activate func CORS

---
+++ frontend/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded',(event) =>{
+    getVisitCount();
+})
+
+const functionApiURL = 'https://getresumecounter-zn.azurewebsites.net/api/GetResumeCounter?code=Zfw2n0w9MvkJQVSVHKIm4S8ZZaYYhqjEnQxHm8FUw5gdAzFug_FzNQ==';
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
+
+const getVisitCount = () => {
+    let count =30;
+    fetch(functionApiURL).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
